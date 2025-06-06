632a03622cf5f44b9af4179d218d0abd0099b493
edd86 <edward.cristob@gmail.com>
2025-06-06 03:26:12 -0400

    Refactor database schema: rename 'Persona' to 'Chats' and 'Chat' to 'Messages' for consistency

--- lib/data/sqlite/sqlite_code.dart
+++ lib/data/sqlite/sqlite_code.dart
@@ -1,15 +1,10 @@
-String createPersona = '''CREATE TABLE IF NOT EXISTS Persona (
+String createChat = '''CREATE TABLE IF NOT EXISTS Chats (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL
-      )''';
-
-String createChat = '''CREATE TABLE IF NOT EXISTS Chat (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nombre TEXT NOT NULL,
+        name TEXT NOT NULL,
         createdAt TEXT NOT NULL
       )''';
 
-String crearMessage = '''CREATE TABLE IF NOT EXISTS Message (
+String crearMessage = '''CREATE TABLE IF NOT EXISTS Messages (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         date TEXT NOT NULL,
         message TEXT NOT NULL,
